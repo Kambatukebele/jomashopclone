d44c396df221e00b1ca79c6f6399c5b5357a4638
kambatukebele <kambacharles11@gmail.com>
2024-12-02 21:54:23 +0100

    update 3 annoucement bar. working on css and animation

--- assets/announcement-bar.js
+++ assets/announcement-bar.js
@@ -1,12 +1,40 @@
-const text = document.querySelector(".annoucement__right--visibleText__text"); // Capturing the text in annoucement
-const hiddenBlockInAnnoucement = document.querySelector(
-  ".annoucement__right--hiddenBlock"
-); // capturing the hidden block in annoucement
+function hiddenBlock() {
+  const text = document.querySelector(".annoucement__right--visibleText__text"); // Capturing the text in annoucement
+  const hiddenBlockInAnnoucement = document.querySelector(
+    ".annoucement__right--hiddenBlock"
+  ); // capturing the hidden block in annoucement
 
-text.addEventListener("click", () => {
-  if (hiddenBlockInAnnoucement.classList.contains("hidden")) {
-    hiddenBlockInAnnoucement.classList.remove("hidden");
-  } else {
-    hiddenBlockInAnnoucement.classList.add("hidden");
-  }
-});
+  text.addEventListener("click", () => {
+    if (hiddenBlockInAnnoucement.classList.contains("hidden")) {
+      hiddenBlockInAnnoucement.classList.remove("hidden");
+    } else {
+      hiddenBlockInAnnoucement.classList.add("hidden");
+    }
+  });
+}
+
+hiddenBlock();
+
+// Slide
+function slideAnnoucement() {
+  // console.log(scrollingDiv);
+  window.addEventListener("load", (e) => {
+    const scrollingDivs = document.querySelectorAll(".annoucement__left--div");
+    // for (let i = 0; i < scrollingDivs.length; i++) {
+    //   const element = array[i];
+
+    // }
+    scrollingDivs.forEach((divs, index) => {
+      let current = 0;
+      console.log(divs[index]);
+
+      setInterval(() => {
+        if (!divs.classList.contains("hidden")) {
+          divs.classList.add("hidden");
+        }
+      }, 5000);
+    });
+  });
+}
+
+slideAnnoucement();
