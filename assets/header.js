9edcac1ec251a3b929d19dc820338ef99fe2cc74
Kambatukebele <kambacharles11@gmail.com>
2024-12-06 14:11:19 +0100

    update 3 for header. adding css and javascript for opening and closing menu on small device with animation

--- assets/header.js
+++ assets/header.js
@@ -1,8 +1,11 @@
 const headerTopMenu = document.querySelector('.header__top--menu');
 const headerMobile = document.querySelector('.header__mobile');
+const headerCloseMenu = document.querySelector('.header__mobile__IconClose-span');
 headerTopMenu.addEventListener('click', () => {
-  if (headerMobile.classList.contains('hidden')) {
-    headerMobile.classList.remove('hidden');
-  } else {
-  }
+  headerMobile.classList.add('openMenuAnnimation');
+  headerMobile.classList.remove('closeMenuAnnimation');
+});
+headerCloseMenu.addEventListener('click', () => {
+  headerMobile.classList.remove('openMenuAnnimation');
+  headerMobile.classList.add('closeMenuAnnimation');
 });
