5e1717447ca15f449b91a1bb8c042ee40a7a3889
Kambatukebele <kambacharles11@gmail.com>
2024-12-06 20:07:22 +0100

    update 5 for header. working on mega menu html and css

--- assets/header.js
+++ assets/header.js
@@ -9,3 +9,22 @@ headerCloseMenu.addEventListener('click', () => {
   headerMobile.classList.remove('openMenuAnnimation');
   headerMobile.classList.add('closeMenuAnnimation');
 });
+
+// Submenu js code
+const subMenu = () => {
+  const subMenuDropdownTitleGroup = document.querySelectorAll('.sub-menu__dropdown--title-group');
+  const subMenuDropdownDescGroup = document.querySelectorAll('.sub-menu__dropdown--desc-group');
+  for (let i = 0; i < subMenuDropdownTitleGroup.length; i++) {
+    subMenuDropdownTitleGroup[i].addEventListener('click', () => {
+      for (let j = 0; j < subMenuDropdownDescGroup.length; j++) {
+        if (i === j) {
+          console.log(subMenuDropdownDescGroup[j]);
+
+          subMenuDropdownDescGroup[j].style.display = 'block';
+        }
+      }
+    });
+  }
+};
+
+subMenu();
