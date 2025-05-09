21c577c7cf69617d7e5ebabb008c2f8d35074f30
Kambatukebele <kambacharles11@gmail.com>
2025-05-09 06:45:05 +0200

    update 7 annoucement bar: fixing page jumping on top

--- assets/announcement-bar.js
+++ assets/announcement-bar.js
@@ -1,26 +1,31 @@
 function announcementSlider() {
   const scrollContainer = document.querySelector('#scrollContainer');
   const slides = document.querySelectorAll('.slide');
-  // Initialize a variable to keep track of which slide is currently shown
   let currentIndex = 0;
 
-  // Function to scroll the current slide
-  function scrollToSlide(index) {
-    //access the index in the slides
-    const targetSlide = slides[index];
-    targetSlide.scrollIntoView({ behavior: 'smooth' });
-  }
+  const slideWidth = slides[0].offsetWidth;
 
   setInterval(() => {
     currentIndex++;
     if (currentIndex >= slides.length) {
       currentIndex = 0;
     }
-    scrollToSlide(currentIndex);
+
+    scrollContainer.scrollTo({
+      left: slideWidth * currentIndex,
+      behavior: 'smooth',
+    });
   }, 3000);
 }
+
 announcementSlider();
 
+/**
+ * This function show and hiding the right div that contains a block with:
+ * an icon phone and text
+ * an icon chat and text
+ * an icon whatsup and text
+ */
 function showingHiddenBlock() {
   const announcementRightVisibleText = document.querySelector('.announcement__right--visibleText'); // right text on the announcement bar
   const hiddenBlock = document.querySelector('.hiddenBlock'); // the hidden block
@@ -32,5 +37,4 @@ function showingHiddenBlock() {
     hiddenBlock.style.animation = 'hiddenBlockReverse 300ms ease-in-out forwards';
   });
 }
-
 showingHiddenBlock();
